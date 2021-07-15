896f00eb387318bdc4b54beea679942b12000527
lifangzhou <fangzhou.li@hexagon.com>
2021-07-15 17:38:49 +0800

    fix:element UI

--- src/plugins/element-plus/index.ts
+++ src/plugins/element-plus/index.ts
@@ -1,83 +1,88 @@
-import { App, Component } from "vue";
-import {
-  ElAffix,
-  ElSkeleton,
-  ElBreadcrumb,
-  ElBreadcrumbItem,
-  ElScrollbar,
-  ElSubmenu,
-  ElButton,
-  ElCol,
-  ElRow,
-  ElSpace,
-  ElDivider,
-  ElCard,
-  ElDropdown,
-  ElDialog,
-  ElMenu,
-  ElMenuItem,
-  ElDropdownItem,
-  ElDropdownMenu,
-  ElIcon,
-  ElInput,
-  ElForm,
-  ElFormItem,
-  ElLoading,
-  ElPopover,
-  ElPopper,
-  ElTooltip,
-  ElDrawer,
-  ElPagination,
-  ElAlert,
-  ElRadioButton,
-  ElRadioGroup
-} from "element-plus";
+import { App } from "vue";
+// import {
+//   ElAffix,
+//   ElSkeleton,
+//   ElBreadcrumb,
+//   ElBreadcrumbItem,
+//   ElScrollbar,
+//   ElSubmenu,
+//   ElButton,
+//   ElCol,
+//   ElRow,
+//   ElSpace,
+//   ElDivider,
+//   ElCard,
+//   ElDropdown,
+//   ElDialog,
+//   ElMenu,
+//   ElMenuItem,
+//   ElDropdownItem,
+//   ElDropdownMenu,
+//   ElIcon,
+//   ElInput,
+//   ElForm,
+//   ElFormItem,
+//   ElLoading,
+//   ElPopover,
+//   ElPopper,
+//   ElTooltip,
+//   ElDrawer,
+//   ElPagination,
+//   ElAlert,
+//   ElRadioButton,
+//   ElRadioGroup,
+//   ElCollapse
+// } from "element-plus";
+import ElementPlus from "element-plus";
+
 import "element-plus/packages/theme-chalk/src/base.scss";
+import "element-plus/lib/theme-chalk/index.css";
+// const components = [
+//   ElAffix,
+//   ElSkeleton,
+//   ElBreadcrumb,
+//   ElBreadcrumbItem,
+//   ElScrollbar,
+//   ElSubmenu,
+//   ElButton,
+//   ElCol,
+//   ElRow,
+//   ElSpace,
+//   ElDivider,
+//   ElCard,
+//   ElDropdown,
+//   ElDialog,
+//   ElMenu,
+//   ElMenuItem,
+//   ElDropdownItem,
+//   ElDropdownMenu,
+//   ElIcon,
+//   ElInput,
+//   ElForm,
+//   ElFormItem,
+//   ElPopover,
+//   ElPopper,
+//   ElTooltip,
+//   ElDrawer,
+//   ElPagination,
+//   ElAlert,
+//   ElRadioButton,
+//   ElRadioGroup,
+//   ElCollapse
+// ];
 
-const components = [
-  ElAffix,
-  ElSkeleton,
-  ElBreadcrumb,
-  ElBreadcrumbItem,
-  ElScrollbar,
-  ElSubmenu,
-  ElButton,
-  ElCol,
-  ElRow,
-  ElSpace,
-  ElDivider,
-  ElCard,
-  ElDropdown,
-  ElDialog,
-  ElMenu,
-  ElMenuItem,
-  ElDropdownItem,
-  ElDropdownMenu,
-  ElIcon,
-  ElInput,
-  ElForm,
-  ElFormItem,
-  ElPopover,
-  ElPopper,
-  ElTooltip,
-  ElDrawer,
-  ElPagination,
-  ElAlert,
-  ElRadioButton,
-  ElRadioGroup
-];
-
-const plugins = [ElLoading];
+// const plugins = [ElLoading];
 
 import ElementLocale from "element-plus/lib/locale";
 import zhLocale from "element-plus/lib/locale/lang/zh-cn";
 ElementLocale.use(zhLocale);
 
 export function useElementPlus(app: App) {
-  components.forEach((component: Component) => {
-    app.component(component.name, component);
-  });
-  plugins.forEach(plugin => {
-    app.use(plugin);
-  });
+  app.use(ElementPlus);
+  // components.forEach((component: Component) => {
+  //   app.component(component.name, component);
+  // });
+  // plugins.forEach(plugin => {
+  //   app.use(plugin);
+  // });
 }
--- src/views/components/video/index.vue
+++ src/views/components/video/index.vue
@@ -1,9 +1,48 @@
 <template>
-  <div id="mse"></div>
+  <div>
+    <div id="mse"></div>
+    <el-collapse
+      v-model="activeNames"
+      @change="handleChange"
+    >
+      <el-collapse-item
+        title="一致性 Consistency"
+        name="1"
+      >
+        <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+        <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+      </el-collapse-item>
+      <el-collapse-item
+        title="可控 Controllability"
+        name="4"
+      >
+        <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+        <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+      </el-collapse-item>
+    </el-collapse>
+    <el-card class="box-card">
+      <template #header>
+        <div class="card-header">
+          <span>卡片名称</span>
+          <el-button
+            class="button"
+            type="text"
+          >操作按钮</el-button>
+        </div>
+      </template>
+      <div
+        v-for="o in 4"
+        :key="o"
+        class="text item"
+      >
+        {{'列表内容 ' + o }}
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script lang="ts">
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import Player from "xgplayer/dist/simple_player";
 import volume from "xgplayer/dist/controls/volume";
 import playbackRate from "xgplayer/dist/controls/playbackRate";
@@ -12,6 +51,10 @@ import { deviceDetection } from "/@/utils/deviceDetection";
 
 export default {
   setup() {
+    const activeNames = ref(["1"]);
+    const handleChange = val => {
+      console.log(val);
+    };
     onMounted(() => {
       new Player({
         id: "mse",
@@ -25,7 +68,10 @@ export default {
         playbackRate: [0.5, 0.75, 1, 1.5, 2] //传入倍速可选数组
       });
     });
-    return {};
+    return {
+      activeNames,
+      handleChange
+    };
   }
 };
 </script>
